779c881e8b1dab7bb77ad52b083599f9b214b5eb
Jeffrey Liu <compile.linker@msa.hinet.net>
2024-04-04 14:52:59 +0800

    重構初版

--- www/js/golight.js
+++ www/js/golight.js
@@ -1,99 +1,99 @@
 /**
- * 
+ * LINE 相關初使化
+ * @param {any} liffId
+ */
+function init(liffId) {
+    liff?.init({
+        liffId
+    }).then(() => {
+        document.getElementById('lineLoginButton').addEventListener('click', function () {
+            lineLogin();
+        });
+    });
+}
+
+/**
+ * 登入 LINE 帳號
+ * @returns
+ */
+function lineLogin() {
+    if (!liff.isLoggedIn()) {
+        console.log('使用者未登入');
+        return liff.login();
+    }
+
+    return getProfile();
+}
+
+/**
+ * 取得 LINE Server 回傳的相關資訊
+ */
+function getProfile() {
+    liff.getProfile().then(profile => {
+        const { userId, displayName } = profile;
+        console.log('使用者: ', userId, displayName);
+        return linee({ lineId: userId, displayName: displayName });
+    });
+}
+
+/**
+ * 查詢 LINE 使用者成員資訊
  * @param {any} body
  * @returns
  */
 function linee(body) {
-    return performanceJson(fetch('/api/lineee', performance('post', 'application/json', body)));
+    return fetchToJson('/api/lineee', 'post', body)
+        .then(data => login({ username: data.username, line: true }));
 }
 
 /**
- * 
+ * 登入 LINE 後查詢該成員項目並暫存起來
  * @param {any} body
  * @returns
  */
 function login(body) {
-    return performanceJson(fetch('/api/login', performance('post', 'application/json', body)));
-}
-
-/**
- * 
- * @param {any} body
- * @returns
- */
-function line_record_list(body) {
-    return performanceJson(fetch('/api/light-record-list', performance('post', 'application/json', body)));
-}
-
-/**
- * 
- * @param {any} method
- * @param {any} headers
- * @param {any} body
- * @returns
- */
-function performance(method, headers, body) {
-    return {
-        method,
-        headers,
-        body
-    };
-}
-
-/**
- * 
- * @param {any} promise
- * @returns
- */
-function performanceJson(promise) {
-    return new Promise(x => promise.then(response => x(response.json())));
-}
-
-(function () {
-    liff?.init({
-        liffId: "2004258463-5P6lPeYA"
-    }).then(() => {
-        document.getElementById('lineLoginButton').addEventListener('click', function () {
-            if (!liff.isLoggedIn()) {
-                console.log('使用者未登入');
-                liff.login();
-            } else {
-                liff.getProfile().then(profile => {
-                    const { userId, displayName } = profile;
-                    console.log('使用者: ', userId, displayName);
-
-                    return;
-                    linee({ lineId: userId, displayName: displayName })                   
-                        .then(data => {
-                            if (data.success) {
-                                login({ username: data.username, line: true })
-                                    .then(data => {
-                                        console.log('123', data)
-                                        if (data.success) {
-                                            const token = data.token;
-                                            line_record_list({})
-                                                .then(data => {
-                                                    if (data.success) {
-                                                        const data = localStorage.getItem("data");
-                                                        const json = JSON.parse(data);
-                                                        json.token = token;
-                                                        localStorage.setItem('data', JSON.stringify(json));
-                                                        window.location = 'https://light.go-free.tw/golight.html';
-                                                    } else {
-                                                        alert('登入失敗，請稍後再試');
-                                                    }
-                                                });
-                                        } else {
-                                            alert('登入失敗，請稍後再試');
-                                        }
-                                    });
-                            }
-                            else {
-
-                            }
-                        });
-                });
-            }
+    return fetchToJson('/api/login', 'post', body)
+        .then(data => {
+            storageMember(data);
+            window.location = 'https://light.go-free.tw/golight.html';
         });
+}
+
+/**
+ * 
+ * @param {any} url
+ * @param {any} method
+ * @param {any} body
+ * @returns
+ */
+function fetchToJson(url, method, body) {
+    return new Promise(resolve => {
+        fetch(url, {
+            method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+            .then(x => resolve(x.json()));
     });
-})();
+}
+
+/**
+ * 
+ * @param {any} data
+ */
+function storageMember(data) {
+    const member = localStorage.getItem('data');
+    const json = JSON.parse(member);
+
+    json.token = data.token;
+    localStorage.setItem('data', JSON.stringify(json));
+}
+
+/**
+ * 執行 LINE 登入
+ */
+(function (liffId) {
+    init(liffId);
+})('2004258463-5P6lPeYA');
